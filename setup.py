492c5dcfcb43a4cc5f7ed1bd0d9ac35ed32d2bbb
dgaiero <dpgaiero@gmail.com>
2017-09-10 17:47:04 -0700

    Froze application 0.2 version

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 import os
 from cx_Freeze import setup, Executable
 
-os.environ['TCL_LIBRARY'] = 'C:/Users/Administrator/AppData/Local/Programs/Python/Python35/tcl/tcl8.6'
-os.environ['TK_LIBRARY'] = 'C:/Users/Administrator/AppData/Local/Programs/Python/Python35/tcl/tk8.6'
+os.environ['TCL_LIBRARY'] = 'C:/Users/Dominic Gaiero/AppData/Local/Programs/Python/Python36-32/tcl/tcl8.6'
+os.environ['TK_LIBRARY'] = 'C:/Users/Dominic Gaiero/AppData/Local/Programs/Python/Python36-32/tcl/tk8.6'
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
 buildOptions = dict(
     packages=[],
     excludes=[],
-    include_files=['icon.ico', 'C:/Users/Administrator/AppData/Local/Programs/Python/Python35/DLLs/tcl86t.dll',
-                   'C:/Users/Administrator/AppData/Local/Programs/Python/Python35/DLLs/tk86t.dll']
+    include_files=['icon.ico', 'C:/Users/Dominic Gaiero/AppData/Local/Programs/Python/Python36-32/DLLs/tcl86t.dll',
+                   'C:/Users/Dominic Gaiero/AppData/Local/Programs/Python/Python36-32/DLLs/tk86t.dll']
 )
 
 import sys
@@ -25,7 +25,7 @@ executables = [
 
 setup(
     name="Resistor Setup",
-    version="0.1",
+    version="0.2",
     description="Generates images from resisitor values",
     author="Dominic Gaiero",
     options=dict(build_exe=buildOptions),
